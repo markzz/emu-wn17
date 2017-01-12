449e2bc0ef781d7be2c5945ec5d59a46341fda9c
Mark Weiman <mark.weiman@markzz.com>
2017-01-12 17:33:02 -0500

    Initial submission HW-01-12

---
+++ HW-01-12/src/LinkedList.java
@@ -0,0 +1,71 @@
+/*
+ * Mark Weiman
+ * https://github.com/markzz/emu-wn17/blob/COSC311/HW-01-10/LinkedList.java
+ * COSC 311
+ * HW 01/12
+ * WINTER 2017
+ */
+
+public class LinkedList {
+    private Node first;
+    private int length;
+
+    private class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public LinkedList(int[] arr) {
+        Node last = null;
+        this.length = 0;
+
+        for (int i : arr) {
+            if (last == null) {
+                last = new Node(i, null);
+                this.first = last;
+            } else {
+                last.next = new Node(i, null);
+                last = last.next;
+            }
+
+            this.length++;
+        }
+    }
+
+    public void rm(int index) {
+        int i;
+        Node prev = this.first;
+
+        if (index < 0 || index >= this.length) return;
+
+        index = this.length - index - 1;
+        
+        if (index == 0) {
+            this.first = prev.next;
+            return;
+        }
+
+        for (i = 1; i <= index; i++) {
+            prev = prev.next;
+        }
+
+        prev.next = prev.next.next;
+    }
+
+    public String to_string() {
+        Node n = this.first;
+        String ret = "";
+
+        while (n != null) {
+            ret += String.format("%3d ", n.data);
+            n = n.next;
+        }
+
+        return ret;
+    }
+}
---
+++ HW-01-12/src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Mark Weiman
+ * https://github.com/markzz/emu-wn17/blob/COSC311/HW-01-10/Main.java
+ * COSC 311
+ * HW 01/10
+ * WINTER 2017
+ */
+
+public class Main {
+    public static void main(String[] argv) {
+        int[] array = {0, 2, 4, 6, 1};
+        int k = 4;
+
+        LinkedList list = new LinkedList(array);
+        System.out.printf("original list: %s\n", list.to_string());
+        list.rm(k);
+        System.out.printf("modified list: %s\n", list.to_string());
+    }
+}
